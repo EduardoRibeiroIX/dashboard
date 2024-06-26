fb2752edb1d1585c1052b574a33459a2d7a0a194
EduardoRibeiroIX <eduardo.ribeiro@itec.ufpa.br>
2024-06-26 17:24:09 -0300

    server

--- site-dashboard/src/components/dashboard/dashboard.tsx
+++ site-dashboard/src/components/dashboard/dashboard.tsx
@@ -1,8 +1,5 @@
 import './dashboard.css'
 import React from 'react';
-import {
-  RadialBarChart, RadialBar, Legend, Tooltip, ResponsiveContainer
-} from 'recharts';
 import { CampaignStatistics } from '../campaignStatistics/CampaignStatistics';
 import { CampaignSuccessRate } from '../campaignSuccessRate/CampaignSuccessRate';
 
@@ -11,12 +8,6 @@ interface DataPoint {
   uv: number;
   fill: string;
 }
-
-const data: DataPoint[] = [
-  {
-    name: '18-24', uv: 31.47, fill: '#8884d8',
-  },
-];
 
 const Dashboard: React.FC = () => {
   return (
